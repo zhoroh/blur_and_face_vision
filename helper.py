64c118c2def6b5e87dea1aaa7d6e67a242d86e3c
Oreoluwa <bolarinwaoreoluwa24@gmail.com>
2024-01-19 21:36:02 +0100

    added files

---
+++ helper.py
@@ -0,0 +1,228 @@
+from ultralytics import YOLO
+import time
+import streamlit as st
+import cv2
+from pytube import YouTube
+
+import settings
+
+
+def load_model(model_path):
+    """
+    Loads a YOLO object detection model from the specified model_path.
+
+    Parameters:
+        model_path (str): The path to the YOLO model file.
+
+    Returns:
+        A YOLO object detection model.
+    """
+    model = YOLO(model_path)
+    return model
+
+
+def display_tracker_options():
+    display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
+    is_display_tracker = True if display_tracker == 'Yes' else False
+    if is_display_tracker:
+        tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
+        return is_display_tracker, tracker_type
+    return is_display_tracker, None
+
+
+def _display_detected_frames(video_writer,conf, model, image,classes, is_display_tracking=None, tracker=None):
+    """
+    Display the detected objects on a video frame using the YOLOv8 model.
+
+    Args:
+    - conf (float): Confidence threshold for object detection.
+    - model (YoloV8): A YOLOv8 object detection model.
+    - st_frame (Streamlit object): A Streamlit object to display the detected video.
+    - image (numpy array): A numpy array representing the video frame.
+    - is_display_tracking (bool): A flag indicating whether to display object tracking (default=None).
+
+    Returns:
+    None
+    """
+
+    
+
+    # Resize the image to a standard size
+    # image = cv2.resize(image, (720, int(720*(9/16))))
+
+    # Display object tracking, if specified
+    if is_display_tracking:
+        res = model.track(image, classes=classes, conf=conf, persist=True, tracker=tracker)
+    else:
+        # Predict the objects in the image using the YOLOv8 model
+        res = model.predict(image,classes=classes, conf=conf)
+
+    # # Plot the detected objects on the video frame
+    res_plotted = res[0].plot()
+    video_writer.write(res_plotted)
+    # st_frame.image(res_plotted,
+    #                caption='Detected Video',
+    #                channels="BGR",
+    #                use_column_width=True
+    #                )
+
+
+def play_youtube_video(conf, model):
+    """
+    Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
+
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    source_youtube = st.sidebar.text_input("YouTube Video url")
+
+    is_display_tracker, tracker = display_tracker_options()
+    selected_options = st.sidebar.multiselect("Choose The classes of objects     you interested in", list(settings.CLASS_NAMES.keys()))
+    if selected_options:
+        message = "You selected:", ", ".join(selected_options)
+        st.sidebar.success(message[0] + " " + message[1])
+        classes_to_detect = [settings.CLASS_NAMES[key] for key in selected_options]
+        if st.sidebar.button('Detect Objects'):
+            st.sidebar.success("Processing Youtube Video......")
+            try:
+                yt = YouTube(source_youtube)
+                stream = yt.streams.filter(file_extension="mp4", res=720).first()
+                vid_cap = cv2.VideoCapture(stream.url)
+                pathToWriteVideo='result_youtube.mp4'
+                fourcc = cv2.VideoWriter_fourcc(*'mpv4')
+                width = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                frames_per_second = vid_cap.get(cv2.CAP_PROP_FPS)
+                total_frames = int(vid_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                if total_frames > 2500:
+                    total_frames = 2500
+                checkpoints = [25, 50, 75, 100]
+                current_frame = 0
+                video_writer = cv2.VideoWriter(pathToWriteVideo, fourcc , fps=float(frames_per_second), frameSize=(width, height), isColor=True)
+                while (vid_cap.isOpened()):
+                    success, image = vid_cap.read()
+                    percent_complete = (current_frame / total_frames) * 100
+                    if checkpoints and percent_complete >= checkpoints[0]:
+                        st.sidebar.success(f"Video Processing is {checkpoints.pop(0)}% completed.")
+                    current_frame += 1
+                    if current_frame < total_frames + 1: # youtube videos can be very very long
+
+                        if success:
+                            _display_detected_frames(video_writer,
+                                                        conf,
+                                                        model,
+                                                        image,
+                                                        classes_to_detect,
+                                                        is_display_tracker,
+                                                        tracker
+                                                        )
+                        
+                        else:
+                            video_writer.release()
+                            vid_cap.release()
+                            break
+                    else:
+                        ""
+                        video_writer.release()
+                        vid_cap.release()
+                        break
+                st_video = open('result_youtube.mp4','rb')
+                video_bytes = st_video.read()
+                st.video(video_bytes)
+                st.write("Detected Video") 
+            except Exception as e:
+                st.sidebar.error("Error loading video: " + str(e))
+    else:
+        st.sidebar.error("No classes Selected")
+
+def play_stored_video(conf, model):
+    """
+    Plays a stored video file. Tracks and detects objects in real-time using the YOLOv8 object detection model.
+
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    source_vid = st.sidebar.selectbox(
+        "Choose a video...", settings.VIDEOS_DICT.keys())
+
+    is_display_tracker, tracker = display_tracker_options()
+
+    with open(settings.VIDEOS_DICT.get(source_vid), 'rb') as video_file:
+        video_bytes = video_file.read()
+    if video_bytes:
+        st.video(video_bytes)
+    
+    selected_options = st.sidebar.multiselect("Choose The classes of objects     you interested in", list(settings.CLASS_NAMES.keys()))
+    if selected_options:
+        message = "You selected:", ", ".join(selected_options)
+        st.sidebar.success(message[0] + " " + message[1])
+        classes_to_detect = [settings.CLASS_NAMES[key] for key in selected_options]
+        if st.sidebar.button('Detect  Objects'):
+            st.sidebar.success("Processing Video......")
+            try:
+                vid_cap = cv2.VideoCapture(
+                    str(settings.VIDEOS_DICT.get(source_vid)))
+                pathToWriteVideo='result.mp4'
+                fourcc = cv2.VideoWriter_fourcc(*'mpv4')
+                width = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                frames_per_second = vid_cap.get(cv2.CAP_PROP_FPS)
+                total_frames = int(vid_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                if total_frames > 2500:
+                    total_frames = 2500
+                checkpoints = [25, 50, 75, 100]
+
+                # Read and process the video frame by frame
+                current_frame = 0
+                video_writer = cv2.VideoWriter(pathToWriteVideo, fourcc , fps=float(frames_per_second), frameSize=(width, height), isColor=True)
+                while (vid_cap.isOpened()):
+                    success, image = vid_cap.read()
+                    percent_complete = (current_frame / total_frames) * 100
+                    if checkpoints and percent_complete >= checkpoints[0]:
+                        st.sidebar.success(f"Video Processing is {checkpoints.pop(0)}% completed.")
+                    current_frame += 1
+                    if current_frame < total_frames + 1: #  videos can be very very long
+
+                        if success:
+                            _display_detected_frames(video_writer,
+                                                        conf,
+                                                        model,
+                                                        image,
+                                                        classes_to_detect,
+                                                        is_display_tracker,
+                                                        tracker
+                                                        )
+                        
+                        else:
+                            video_writer.release()
+                            vid_cap.release()
+                            break
+                    else:
+                        ""
+                        video_writer.release()
+                        vid_cap.release()
+                        break
+                st_video = open('result.mp4','rb')
+                video_bytes = st_video.read()
+                st.video(video_bytes)
+                st.write("Detected Video") 
+            except Exception as e:
+                st.sidebar.error("Error loading video: " + str(e))
+
+    else:
+        st.sidebar.error("No classes Selected")
+    
---
+++ settings.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+import sys
+
+# Get the absolute path of the current file
+file_path = Path(__file__).resolve()
+
+# Get the parent directory of the current file
+root_path = file_path.parent
+
+# Add the root path to the sys.path list if it is not already there
+if root_path not in sys.path:
+    sys.path.append(str(root_path))
+
+# Get the relative path of the root directory with respect to the current working directory
+ROOT = root_path.relative_to(Path.cwd())
+
+# Sources
+IMAGE = 'Image'
+VIDEO = 'Video'
+YOUTUBE = 'YouTube'
+
+SOURCES_LIST = [IMAGE, VIDEO, YOUTUBE]
+
+# Images config
+IMAGES_DIR = ROOT / 'images'
+DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+
+# Videos config
+VIDEO_DIR = ROOT / 'videos'
+VIDEO_1_PATH = VIDEO_DIR / 'cars.mp4'
+VIDEO_2_PATH = VIDEO_DIR / 'people_1.mp4'
+VIDEO_3_PATH = VIDEO_DIR / 'people_car.mp4'
+VIDEOS_DICT = {
+    'cars': VIDEO_1_PATH,
+    'people_1': VIDEO_2_PATH,
+    'people_car': VIDEO_3_PATH,
+}
+
+# ML Model config
+MODEL_DIR = ROOT / 'weights'
+DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
+# In case of your custome model comment out the line above and
+# Place your custom model pt file name at the line below 
+# DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
+
+SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
+
+
+CLASS_NAMES= {'person': 0,
+ 'bicycle': 1,
+ 'car': 2,
+ 'motorcycle': 3,
+ 'airplane': 4,
+ 'bus': 5,
+ 'train': 6,
+ 'truck': 7,
+ 'boat': 8,
+ 'traffic light': 9,
+ 'fire hydrant': 10,
+ 'stop sign': 11,
+ 'parking meter': 12,
+ 'bench': 13,
+ 'bird': 14,
+ 'cat': 15,
+ 'dog': 16,
+ 'horse': 17,
+ 'sheep': 18,
+ 'cow': 19,
+ 'elephant': 20,
+ 'bear': 21,
+ 'zebra': 22,
+ 'giraffe': 23,
+ 'backpack': 24,
+ 'umbrella': 25,
+ 'handbag': 26,
+ 'tie': 27,
+ 'suitcase': 28,
+ 'frisbee': 29,
+ 'skis': 30,
+ 'snowboard': 31,
+ 'sports ball': 32,
+ 'kite': 33,
+ 'baseball bat': 34,
+ 'baseball glove': 35,
+ 'skateboard': 36,
+ 'surfboard': 37,
+ 'tennis racket': 38,
+ 'bottle': 39,
+ 'wine glass': 40,
+ 'cup': 41,
+ 'fork': 42,
+ 'knife': 43,
+ 'spoon': 44,
+ 'bowl': 45,
+ 'banana': 46,
+ 'apple': 47,
+ 'sandwich': 48,
+ 'orange': 49,
+ 'broccoli': 50,
+ 'carrot': 51,
+ 'hot dog': 52,
+ 'pizza': 53,
+ 'donut': 54,
+ 'cake': 55,
+ 'chair': 56,
+ 'couch': 57,
+ 'potted plant': 58,
+ 'bed': 59,
+ 'dining table': 60,
+ 'toilet': 61,
+ 'tv': 62,
+ 'laptop': 63,
+ 'mouse': 64,
+ 'remote': 65,
+ 'keyboard': 66,
+ 'cell phone': 67,
+ 'microwave': 68,
+ 'oven': 69,
+ 'toaster': 70,
+ 'sink': 71,
+ 'refrigerator': 72,
+ 'book': 73,
+ 'clock': 74,
+ 'vase': 75,
+ 'scissors': 76,
+ 'teddy bear': 77,
+ 'hair drier': 78,
+ 'toothbrush': 79}
+
