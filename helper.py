4065bf92d81699d1f78144909c71aad33d863d7f
Oreoluwa <bolarinwaoreoluwa24@gmail.com>
2024-01-20 12:43:49 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -179,7 +179,7 @@ def play_stored_video(conf, model):
             try:
                 vid_cap = cv2.VideoCapture(
                     str(settings.VIDEOS_DICT.get(source_vid)))
-                pathToWriteVideo = 'output.mp4'
+                pathToWriteVideo = 'result.mp4'
                 fourcc = cv2.VideoWriter_fourcc(*'mpv4')
                 width = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                 height = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -219,7 +219,7 @@ def play_stored_video(conf, model):
                         video_writer.release()
                         vid_cap.release()
                         break
-                st_video = open('./'+pathToWriteVideo,'rb')
+                st_video = open(pathToWriteVideo,'rb')
                 video_bytes = st_video.read()
                 st.video(video_bytes)
                 st.write("Detected Video") 
